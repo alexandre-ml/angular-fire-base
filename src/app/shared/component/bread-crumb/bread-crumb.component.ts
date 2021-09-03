9f811547f38d5ae57d2991484e902a67b1dab2e1
Alexandre Lima <alexandre87ml@gmail.com>
2021-09-03 10:09:41 -0300

    bread-crumb Component && refat form/list

---
+++ src/app/shared/component/bread-crumb/bread-crumb.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+interface BreadCrumbItem{
+  text: string;
+  link?: string;
+}
+
+@Component({
+  selector: 'app-bread-crumb',
+  templateUrl: './bread-crumb.component.html',
+  styleUrls: ['./bread-crumb.component.css']
+})
+export class BreadCrumbComponent implements OnInit {
+
+  @Input() items: Array<BreadCrumbItem> = [];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  isTheLastItem(item: BreadCrumbItem): boolean {
+    const i = this.items.indexOf(item);
+
+    return i + 1 == this.items.length;
+  }
+}
